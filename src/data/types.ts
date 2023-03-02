a7aebf69992316796ec54afc995fdc9c10a278ee
Anna Marrodán <anna.marrodan@gmail.com>
2023-03-02 01:40:29 +0100

    Modify types to create OrderedMac type Pick

--- src/data/types.ts
+++ src/data/types.ts
@@ -10,9 +10,14 @@ export type MacsStructure = MacStructure[];
 
 export interface Order {
   id: string;
-  listOfProducts: string[];
+  listOfProducts: OrderedMac[];
   customer: string;
   state: boolean;
 }
 
 export type Orders = Order[];
+
+export type OrderedMac = Pick<
+  MacStructure,
+  "id" | "productName" | "price" | "image"
+>;
